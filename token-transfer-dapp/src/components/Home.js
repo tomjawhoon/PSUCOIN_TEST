8ceb1865568a424a5382f3ba91f65dd79031916e
Naratorn <tomjawhoon@hotmail.com>
2019-05-24 09:16:03 +0700

    first commit

---
+++ token-transfer-dapp/src/components/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Layout from './Layout';
+export default class Home extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render () {
+        return (
+         <div>
+              {/* hello home */}
+        <Layout/>
+         </div>  
+        );
+    }
+}
